5b09993bd3f7c03b42c67fa40498269a34b15361
irfo1 <irfosabanovic@gmail.com>
2022-12-10 00:16:08 +0100

    micronutrient class

---
+++ src/main/java/ba/rpr/domain/Micronutrient.java
@@ -0,0 +1,43 @@
+package ba.rpr.domain;
+
+/**
+ * Class used to represent single micronutrient record from database
+ */
+public class Micronutrient {
+    private int id;
+    private String name;
+    private String role;
+    private boolean isVitamin;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isVitamin() {
+        return isVitamin;
+    }
+
+    public void setVitamin(boolean vitamin) {
+        isVitamin = vitamin;
+    }
+}
