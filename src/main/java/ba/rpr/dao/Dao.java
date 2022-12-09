0bd8db25072c4efc492f18e2c6e47162f97926ed
irfo1 <irfosabanovic@gmail.com>
2022-12-10 00:52:00 +0100

    added main dao interface

---
+++ src/main/java/ba/rpr/dao/Dao.java
@@ -0,0 +1,40 @@
+package ba.rpr.dao;
+
+import java.util.List;
+
+/**
+ * Root interface for all Dao classes
+ */
+public interface Dao<T> {
+    /**
+     * Returns entity from database based on id
+     * @param id - the id of the entity
+     * @return entity that has the same id
+     */
+    T getById(int id);
+
+    /**
+     * Adds entity to database
+     * @param item - entity to be added to database
+     */
+    void add(T item);
+
+    /**
+     * Deletes entity from database based on id
+     * @param id - id of entity to be deleted
+     */
+    void delete(int id);
+
+    /**
+     * Updates the entity with the same id
+     * @param id - id of the entity to be updated
+     * @param item - object that contains updates for the entity
+     */
+    void update(int id, T item);
+
+    /**
+     * Returns all entities inside database
+     * @return all entities inside database
+     */
+    List<T> getAll();
+}
