dc9b9bad230b0c6ebbc79620e7009a56264c6ad2
irfo1 <irfosabanovic@gmail.com>
2022-12-10 00:18:26 +0100

    source class

---
+++ src/main/java/ba/rpr/domain/Source.java
@@ -0,0 +1,25 @@
+package ba.rpr.domain;
+
+/**
+ * class used to represent single source record from database
+ */
+public class Source {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
