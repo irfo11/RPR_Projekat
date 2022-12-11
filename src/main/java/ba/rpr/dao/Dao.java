65cede1483975ae66fe8bec67a0ffcf7c8a7ece4
irfo1 <irfosabanovic@gmail.com>
2022-12-11 22:17:32 +0100

    javadoc fix

--- src/main/java/ba/rpr/dao/Dao.java
+++ src/main/java/ba/rpr/dao/Dao.java
@@ -7,26 +7,30 @@ import java.util.List;
  */
 public interface Dao<T> {
     /**
-     * Returns entity from database based on id
+     * Returns entity from database based on given id, null if there is no element with the same id
+     *
      * @param id - the id of the entity
-     * @return entity that has the same id
+     * @return entity that has the same id, null if there is no element with the same id
      */
     T getById(int id);
 
     /**
      * Adds entity to database
+     *
      * @param item - entity to be added to database
      */
     void add(T item);
 
     /**
      * Deletes entity from database based on id
+     *
      * @param id - id of entity to be deleted
      */
     void delete(int id);
 
     /**
      * Updates the entity with the same id
+     *
      * @param id - id of the entity to be updated
      * @param item - object that contains updates for the entity
      */
@@ -34,6 +38,7 @@ public interface Dao<T> {
 
     /**
      * Returns all entities inside database
+     *
      * @return all entities inside database
      */
     List<T> getAll();
