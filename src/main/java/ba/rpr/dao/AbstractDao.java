72c4730b601365d8dbbf09c9cc7b1948c4c37367
irfo1 <irfosabanovic@gmail.com>
2022-12-27 12:39:50 +0100

    refactoring, abstract dao class

---
+++ src/main/java/ba/rpr/dao/AbstractDao.java
@@ -0,0 +1,34 @@
+package ba.rpr.dao;
+
+import java.sql.*;
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Properties;
+
+import ba.rpr.dao.exceptions.DaoException;
+
+/*svaka sql implementacija ce imati svoj string (ovo je vezano za singleton konekciju)*/
+public abstract class AbstractDao<T> implements Dao<T>{
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
