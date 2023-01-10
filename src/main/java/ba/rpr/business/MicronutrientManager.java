9327e6a8f51b9d5e05b5170ac1c8788c093b2bc2
irfo1 <irfosabanovic@gmail.com>
2023-01-10 23:05:44 +0100

    micronutrient manager, better name validation

--- src/main/java/ba/rpr/business/MicronutrientManager.java
+++ src/main/java/ba/rpr/business/MicronutrientManager.java
@@ -1,6 +1,5 @@
 package ba.rpr.business;
 
-import ba.rpr.dao.Dao;
 import ba.rpr.dao.DaoFactory;
 import ba.rpr.dao.exceptions.DaoException;
 import ba.rpr.domain.Micronutrient;
@@ -19,8 +18,11 @@ public class MicronutrientManager {
     }
 
     public void add(Micronutrient micronutrient) throws DaoException {
-        if(micronutrient == null || micronutrient.getName().length() == 0 || micronutrient.getName().split(" ").length > 1)
-            throw new DaoException("Micronutrient name must be one word, do not add vitamin before name");
+        if(micronutrient == null || micronutrient.getName().length() < 1 ||micronutrient.getName().length() > 45)
+            throw new DaoException("Micronutrient must have name length between 3 to 45 characters");
+        for(String token: micronutrient.getName().split(" "))
+            if(token.equalsIgnoreCase("vitamin"))
+                throw new DaoException("Micronutrient does not need 'vitamin' in its name");
         try {
             DaoFactory.micronutrientDao().add(micronutrient);
         } catch(DaoException e) {
@@ -41,8 +43,11 @@ public class MicronutrientManager {
     }
 
     public void update(int id, Micronutrient micronutrient) throws DaoException {
-        if(micronutrient == null || micronutrient.getName().length() == 0 || micronutrient.getName().split(" ").length > 1)
-            throw new DaoException("Micronutrient name must be one word, do not add vitamin before name");
+        if(micronutrient == null || micronutrient.getName().length() < 1 ||micronutrient.getName().length() > 45)
+            throw new DaoException("Micronutrient must have name length between 3 to 45 characters");
+        for(String token: micronutrient.getName().split(" "))
+            if(token.equalsIgnoreCase("vitamin"))
+                throw new DaoException("Micronutrient does not need 'vitamin' in its name");
         try {
             DaoFactory.micronutrientDao().update(id, micronutrient);
         } catch(DaoException e) {
@@ -59,6 +64,11 @@ public class MicronutrientManager {
     }
 
     public Micronutrient searchByName(String name) throws DaoException {
+        if(name == null || name.length() < 1 || name.length() > 45)
+            throw new DaoException("Micronutrient must have name length between 3 to 45 characters");
+        for(String token: name.split(" "))
+            if(token.equalsIgnoreCase("vitamin"))
+                throw new DaoException("Micronutrient does not need 'vitamin' in its name");
         if(name == null || name.length() == 0 || name.split(" ").length > 1)
             throw new DaoException("Micronutrient name must be one word, do not add vitamin before name");
         Micronutrient micronutrient = DaoFactory.micronutrientDao().searchByName(name);
@@ -66,4 +76,5 @@ public class MicronutrientManager {
             throw new DaoException("Micronutrient with the name=" + name + " does not exist");
         return micronutrient;
     }
+
 }
