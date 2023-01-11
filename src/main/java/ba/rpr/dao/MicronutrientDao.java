a5e8115c2faed4588f2e2a998a641abb2587e53b
irfo1 <irfosabanovic@gmail.com>
2023-01-11 22:26:18 +0100

    javadoc fix

--- src/main/java/ba/rpr/dao/MicronutrientDao.java
+++ src/main/java/ba/rpr/dao/MicronutrientDao.java
@@ -8,11 +8,11 @@ import ba.rpr.dao.exceptions.DaoException;
  */
 public interface MicronutrientDao extends Dao<Micronutrient> {
     /**
-     * Returns micronutrient with the same name given as parameter
+     * Returns micronutrient with the same name given as parameter, null if there is no element with the same name
      *
      * @param name - name of the micronutrient to be returned
-     * @return micronutrient with the same name given as parameter
-     * @throws DaoException - if element with given name can't be found in database
+     * @return micronutrient with the same name given as parameter, null if there is no element with the same name
+     * @throws DaoException - if there are problems with database server
      */
     Micronutrient searchByName(String name) throws DaoException;
 }
