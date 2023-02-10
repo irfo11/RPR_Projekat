f8d68e2e6bd3c8d585130a494b88863148ea290e
irfo1 <irfosabanovic@gmail.com>
2023-02-10 14:18:54 +0100

    added model

--- src/main/java/ba/rpr/controllers/MicronutrientEditController.java
+++ src/main/java/ba/rpr/controllers/MicronutrientEditController.java
@@ -1,46 +1,80 @@
 package ba.rpr.controllers;
 
+import ba.rpr.business.MicronutrientManager;
+import ba.rpr.dao.exceptions.DaoException;
 import ba.rpr.domain.Micronutrient;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class MicronutrientEditController {
+    private final Integer id;
+    private MicronutrientModel micronutrientModel = new MicronutrientModel();
+
+    private final MicronutrientManager micronutrientManager = new MicronutrientManager();
+
+    public Scene scene;
     public ToggleGroup micronutrientType;
     public TextField nameTextField;
     public TextArea roleTextArea;
-    public Micronutrient micronutrient;
-    public boolean edit;
+    public Button actionButton;
 
-    public MicronutrientEditController(Micronutrient micronutrient, boolean edit) {
-        this.micronutrient = micronutrient;
-        this.edit = edit;
+    public MicronutrientEditController(Integer id) {
+        this.id = id;
     }
 
     @FXML
     public void initialize() {
-        if(edit) {
-            nameTextField.setText(micronutrient.getName());
-            roleTextArea.setText(micronutrient.getRole());
-            if(micronutrient.isVitamin()) {
-                micronutrientType.getToggles().get(0).setSelected(true);
-            } else {
-                micronutrientType.getToggles().get(1).setSelected(true);
-            }
+        try {
+            nameTextField.textProperty().bindBidirectional(micronutrientModel.name);
+            roleTextArea.textProperty().bindBidirectional(micronutrientModel.role);
+            ((RadioButton) micronutrientType.getToggles().get(0)).setOnAction(actionEvent -> {
+                micronutrientModel.isVitamin.set(true);
+            });
+            ((RadioButton) micronutrientType.getToggles().get(1)).setOnAction(actionEvent -> {
+                micronutrientModel.isVitamin.set(false);
+            });
+            if (id != null) {
+                micronutrientModel.fromMicronutrient(micronutrientManager.getById(id));
+                if (micronutrientModel.isVitamin.get()) {
+                    micronutrientType.getToggles().get(0).setSelected(true);
+                } else {
+                    micronutrientType.getToggles().get(1).setSelected(true);
+                }
+                actionButton.setText("Update");
+            } else actionButton.setText("Add");
+        } catch(DaoException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
         }
     }
 
-    public void buttonClicked(ActionEvent actionEvent) {
-        micronutrient.setName(nameTextField.getText());
-        micronutrient.setRole(roleTextArea.getText());
-        if(micronutrientType.getSelectedToggle().toString().contains("Vitamin")) micronutrient.setVitamin(true);
-        else micronutrient.setVitamin(false);
-        Node node = (Node) actionEvent.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-        stage.close();
+    public void save(ActionEvent actionEvent) {
+        try {
+            if(id != null) micronutrientManager.update(id, micronutrientModel.toMicronutrient());
+            else micronutrientManager.add(micronutrientModel.toMicronutrient());
+            scene.getWindow().hide();
+        } catch (DaoException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
+        }
+    }
+
+    public class MicronutrientModel {
+        public SimpleStringProperty name = new SimpleStringProperty("");
+        public SimpleStringProperty role = new SimpleStringProperty("");
+        public SimpleBooleanProperty isVitamin = new SimpleBooleanProperty(true);
+
+        public void fromMicronutrient(Micronutrient micronutrient) {
+            name.set(micronutrient.getName());
+            role.set(micronutrient.getRole());
+            isVitamin.set(micronutrient.isVitamin());
+        }
+
+        public Micronutrient toMicronutrient() {
+            return new Micronutrient(-1, name.get(), role.get(), isVitamin.get());
+        }
     }
 }
