9b2b9638f1949728b887a5ccf99cbecbbb048953
irfo1 <irfosabanovic@gmail.com>
2023-02-10 14:18:58 +0100

    added model

--- src/main/java/ba/rpr/controllers/SourceEditController.java
+++ src/main/java/ba/rpr/controllers/SourceEditController.java
@@ -1,33 +1,65 @@
 package ba.rpr.controllers;
 
+import ba.rpr.business.SourceManager;
+import ba.rpr.dao.exceptions.DaoException;
 import ba.rpr.domain.Source;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-/*Uradi onHide metodu kada se sa X izade*/
 public class SourceEditController {
-    private Source source;
-    private boolean edit;
-    public TextField nameTextField;
+    private final Integer id;
 
-    public SourceEditController(Source source, boolean edit) {
-        this.source = source;
-        this.edit = edit;
+    private SourceModel sourceModel = new SourceModel();
+
+    private final SourceManager sourceManager = new SourceManager();
+
+    public Scene scene;
+    public TextField nameTextField;
+    public Button actionButton;
+
+
+    public SourceEditController(Integer id) {
+        this.id = id;
     }
 
     @FXML
     public void initialize() {
-        if(edit) nameTextField.setText(source.getName());
+        try {
+            nameTextField.textProperty().bindBidirectional(sourceModel.name);
+            if (id != null) {
+                sourceModel.fromSource(sourceManager.getById(id));
+                actionButton.setText("Update");
+            } else actionButton.setText("Add");
+        } catch(DaoException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
+        }
     }
 
-    public void buttonClicked(ActionEvent actionEvent) {
-        source.setName(nameTextField.getText());
-        Node source = (Node) actionEvent.getSource();
-        Stage stage = (Stage) source.getScene().getWindow();
-        stage.close();
+    public void save(ActionEvent actionEvent) {
+        try {
+            if(id != null) sourceManager.update(id, sourceModel.toSource());
+            else sourceManager.add(sourceModel.toSource());
+            scene.getWindow().hide();
+        } catch(DaoException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).showAndWait();
+        }
     }
 
+    public class SourceModel {
+        public SimpleStringProperty name = new SimpleStringProperty("");
+
+        public void fromSource(Source source) {
+            name.set(source.getName());
+        }
+
+        public Source toSource() {
+            return new Source(-1, name.get());
+        }
+    }
 }
